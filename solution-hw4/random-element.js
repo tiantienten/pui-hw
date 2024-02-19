f20563ebbaa816524fc897d27810d9980b937fa0
tiantienten <tianzez@andrew.cmu.edu>
2024-02-19 16:06:37 -0500

    step 1-6

---
+++ solution-hw4/random-element.js
@@ -0,0 +1,51 @@
+let glazingOptions = {
+    "Keep Original": 0,
+    "Sugar Milk": 0,
+    "Vanilla Milk": 0.5,
+    "Double Chocolate": 1.5,
+};
+
+let packSizeOptions = {
+    1: 1,
+    3: 3,
+    6: 5,
+    12: 10,
+};
+
+
+const queryString = window.location.search;
+const params = new URLSearchParams(queryString);
+const rollType = params.get('roll');
+const Price = rolls[rollType]["basePrice"];
+const ImageFile = rolls[rollType]["imageFile"];
+const title = document.querySelector('.title');
+title.textContent = rollType + " Cinnamon Roll";
+const productImg = document.querySelector(".productImg");
+productImg.src = "../assets/products/" + ImageFile;
+
+
+
+let glazingDropdown = document.querySelector('#glaze');
+let packSizeDropdown = document.querySelector('#quantity');
+let price = document.querySelector("#priceDisplay");
+let selectedQuantity = packSizeDropdown.value;
+let selectedGlazing = glazingDropdown.value;
+
+function onDropDownChange() {
+    selectedQuantity = packSizeDropdown.value;
+    selectedGlazing = glazingDropdown.value;
+    calculatePrice();
+}
+
+function calculatePrice (){
+    const glazingPrice = glazingOptions[selectedGlazing];
+    const packPrice = packSizeOptions[selectedQuantity];
+    const basePrice = Price;
+    finalPrice = (basePrice + glazingPrice) * packPrice;
+    price.textContent = `Price: $${finalPrice.toFixed(2)}`;
+}
+
+glazingDropdown.addEventListener("change", onDropDownChange);
+packSizeDropdown.addEventListener("change", onDropDownChange);
+calculatePrice();
+
---
+++ solution-hw4/rollsData.js
@@ -0,0 +1,41 @@
+const rolls = {
+    "Original": {
+        "basePrice": 2.49,
+        "imageFile": "original-cinnamon-roll.jpg"
+    },
+    "Apple": {
+        "basePrice": 3.49,
+        "imageFile": "apple-cinnamon-roll.jpg"
+    },
+    "Raisin": {
+        "basePrice": 2.99,
+        "imageFile": "raisin-cinnamon-roll.jpg"
+    },
+    "Walnut": {
+        "basePrice": 3.49,
+        "imageFile": "walnut-cinnamon-roll.jpg"
+    },
+    "Double-Chocolate": {
+        "basePrice": 3.99,
+        "imageFile": "double-chocolate-cinnamon-roll.jpg"
+    },
+    "Strawberry": {
+        "basePrice": 3.99,
+        "imageFile": "strawberry-cinnamon-roll.jpg"
+    }    
+};
+
+
+/*
+function updateURL(){
+    const queryString = window.location.search;
+
+    console.log(queryString);
+    
+    const params = new URLSearchParams(queryString);
+    
+    console.log(params);
+    
+    params.append("roll", flavor);
+}
+*/
